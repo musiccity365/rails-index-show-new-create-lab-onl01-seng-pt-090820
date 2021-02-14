cf562248542908746daea40542bb338a68d89d55
musiccity365 <m_sackstein@yahoo.com>
2021-02-14 17:19:09 -0500

    --- Auto-Git Commit ---

---
+++ spec/requests/coupons_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Coupons", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/coupons/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/coupons/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/coupons/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/coupons/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
